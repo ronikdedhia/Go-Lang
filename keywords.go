fe079a11952096244cebc9a0a12fc6fcbce335c7
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2023-04-25 00:21:08 +0530

    basic keywords

---
+++ keywords.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+	var Pname = "Ronik Dedhia"
+	var Lname = "Go Language"
+	var Cname = "Keywords"
+	fmt.Printf("User name: %s", Pname)
+	fmt.Printf("\nLanguage name: %s", Lname)
+	fmt.Printf("\nChapter name: %s", Cname)
+}
