78534dc80960dee66e5497758a3b52f0b0b82e9a
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2023-04-27 00:43:08 +0530

    loop printing strings

---
+++ string-print-loop.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func display(str string) {
+	for w := 0; w < 6; w++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(str)
+	}
+}
+
+func main() {
+	go display("Ronik")
+	display("Dedhia")
+}
